21e235b87bc23cd997cb8bc0fba4655b3581403b
Arvind Maran <234238@shakopeeschools.org>
2019-11-22 13:06:20 -0600

    Update team5.py

--- team5.py
+++ team5.py
@@ -6,10 +6,6 @@
 #     move: A function that returns 'c' or 'b'
 ####
 
-team_name = 'Team Trees' # Only 10 chars displayed.
-strategy_name = 'Nice Copier'
-strategy_description = 'Starts with colludes.Copies what the person did in their last move. After 3 betrays tries collude one more time'
-     
 def move(my_history, their_history, my_score, their_score):
     ''' Arguments accepted: my_history, their_history are strings.
     my_score, their_score are ints.
@@ -18,34 +14,15 @@ def move(my_history, their_history, my_score, their_score):
     Returns 'c' or 'b'. 
     '''
 
-    # my_history: a string with one letter (c or b) per round that has been played with this opponent.
-    # their_history: a string of the same length as history, possibly empty. 
-    # The first round between these two players is my_history[0] and their_history[0].
-    # The most recent round is my_history[-1] and their_history[-1].
-    
-    # Analyze my_history and their_history and/or my_score and their_score.
-    # Decide whether to return 'c' or 'b'.
     
     if len(my_history) == 0:
-        return 'b'
-    if their_history[-10:-1] == ['c','c', 'c', 'c', 'c', 'c', 'c','c', 'c', 'c']:
-        return 'b' 
-    if their_history[-1] == 'b':
-        return 'b'
-    if their_history[-1] == 'c':
         return 'c'
-    if len(my_history) == 99:
-        return 'b'
-    if my_history == ['b', 'b', 'b', 'b', 'b']:
         return 'c'
     
     
-    
+ 
 def test_move(my_history, their_history, my_score, their_score, result):
-    '''calls move(my_history, their_history, my_score, their_score)
-    from this module. Prints error if return value != result.
-    Returns True or False, dpending on whether result was as expected.
-    '''
+    
     real_result = move(my_history, their_history, my_score, their_score)
     if real_result == result:
         return True
